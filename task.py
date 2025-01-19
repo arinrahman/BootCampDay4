a05c02afa26af970c2f2ab04a2ab6030d44b1bfb
arinrahman <rahman.arin29@gmail.com>
2025-01-19 15:56:56 -0700

    Random Module

---
+++ task.py
@@ -0,0 +1,17 @@
+#random module
+import random
+#num=random.randint(1,10)
+#print(num)
+
+#random_number_0_1= random.random()
+#print(random_number_0_1)
+
+#random_float=random.uniform(1,10)
+#print(random_float)
+
+res=random.randint(0,1)
+
+if res==1:
+    print("Heads!")
+else:
+    print("Tails!")
